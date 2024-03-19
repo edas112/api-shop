c8bae89d016924789ebb076f778a0f2cdabcb510
edas112 <you@example.com>
2024-03-19 22:03:45 +0200

    getallproduct

--- controllers/getAllProduct.js
+++ controllers/getAllProduct.js
@@ -1,5 +1,9 @@
-function getAllProduct() {
-  return 'tada';
+const Product = require('../models/creatModel');
+
+async function getAllProduct() {
+  const response = await Product.find({});
+
+  return response;
 }
 
 module.exports = getAllProduct;
--- routes/productGetterRoutes.js
+++ routes/productGetterRoutes.js
@@ -4,14 +4,25 @@ const { getAllProduct, getProductById } = require('../controllers');
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  const data = getAllProduct();
-  res.json(data);
+router.get('/', async (req, res) => {
+  try {
+    const data = await getAllProduct(req.body);
+    res.json(data);
+  } catch (error) {
+    console.log('error', error.message);
+    res.status(400).json({ error: error.message });
+  }
 });
 
-router.get('/:id', (req, res) => {
-  const data = getProductById();
-  res.json(data);
+router.get('/:id', async (req, res) => {
+  try {
+    console.log(req.params);
+    const data = await getProductById(req.params.id);
+    res.json(data);
+  } catch (error) {
+    console.log('error', error.message);
+    res.status(400).json({ error: error.message });
+  }
 });
 
 module.exports = router;
